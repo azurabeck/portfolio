e366c1649633489ff3e898eef89cee761ebb8411
Rebecca <rebecca@psafe.com>
2018-11-06 20:27:36 -0200

    front page

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import {Grid, Row, Col} from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faGithub, faPinterest, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 class Header extends Component {
     render() {
@@ -48,21 +50,39 @@ class Header extends Component {
 
                 </div>
 
-            <div class="welcome-image-area">
-                <div class="display-table">
-                    <div class="display-table-cell">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="header-text text-center">
-                                        <h1 class="typewrite">I am Developer</h1>         
-                                    </div>
-                                </div>
-                            </div>
+                <div class="welcome-image-area">
+                    <div class="display-table">
+                        <div class="display-table-cell">
+
+                            <label className='titleHome'>
+                              <span className='work'>WORK</span><br />                                
+                              <span className='be'> BE</span>       
+                              <span className='to'> TO</span>  <br />  
+                              <span className='different'> DI<span className='f'>F</span>FERENT</span>                    
+                            </label>                   
+                                                     
+                            <ul className='socialLink'>
+                                <li><FontAwesomeIcon icon={faGithub} /></li>
+                                <li><FontAwesomeIcon icon={faPinterest} /></li>
+                                <li><FontAwesomeIcon icon={faLinkedin} /></li>
+                            </ul>     
                         </div>
                     </div>
                 </div>
-            </div>
+
+
+                <div className='header-footer-area'>
+                    <div class="arrowSliding">
+                        <div class="arrow"></div>
+                    </div>
+                    <div class="arrowSliding delay1">
+                        <div class="arrow"></div>
+                    </div>
+                    <div class="arrowSliding delay2">
+                        <div class="arrow"></div>
+                    </div>
+                </div>
+           
 
             </header>
         )
